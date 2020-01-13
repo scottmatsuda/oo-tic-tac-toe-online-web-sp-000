a65288f179410f1eb1d033d50f4aa575171f5dfe
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-13 05:49:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,27 +59,27 @@ def current_player
 end
 
 def won?
-if @board == [" ", " ", " ", " ", " ", " ", " ", " ", " "] 
-  return false
-end
-WIN_COMBINATIONS.each do |win_combination|
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-
-  position_1 = @board[win_index_1] 
-  position_2 = @board[win_index_2] 
-  position_3 = @board[win_index_3] 
-
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination
+  if @board == [" ", " ", " ", " ", " ", " ", " ", " ", " "] 
+    return false
+  end
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+  
+    position_1 = @board[win_index_1] 
+    position_2 = @board[win_index_2] 
+    position_3 = @board[win_index_3] 
+  
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    end
+  
+    if position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    end
   end
   
-  if position_1 == "O" && position_2 == "O" && position_3 == "O"
-    return win_combination
+  return false
   end
-end
-
-return false
-end
 end
